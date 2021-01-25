8f69e66a48c599ba1af258cdbcfd2fd9b29d0f6d
Matthew Easley <matteasley8791@gmail.com>
2021-01-25 17:26:06 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -11,10 +11,12 @@ final_hash
 end
 
 def get_japanese_emoticon(path, emoticon)
-  load_library(path).each |key, value|
+  load_library(path).each do |key, value|
   if value [:english] == emoticon
     return value[:japanese]
-  
+  end 
+end
+return "Sorry, that emoticon was not found"
 end
 
 def get_english_meaning(path, emoticon)
